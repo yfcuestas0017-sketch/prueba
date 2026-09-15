3ecb19bf0d19b516acc8a3e397d15c7dcfd035b8
yimmy ferney cuestas coral <yfcuestas.0017@unicesmag.edu.co>
2026-09-14 23:06:45 -0500

    Primer commit 5

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -3,7 +3,7 @@ import { login, register } from '../controllers/auth.controller.js';
 
 const router = Router();
 
-router.post('/login', login);
-router.post('/register', register);
+router.post('/auth/login', login);
+router.post('/auth/register', register);
 
 export default router;
