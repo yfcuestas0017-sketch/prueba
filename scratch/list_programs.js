55a53a83a1be83e9b8b23b20072089a87ee32f13
YIMMY <yfcuestas.0017@unicesmag.edu.co>
2026-09-22 23:17:00 -0500

    Delete scratch directory

--- scratch/list_programs.js
+++
@@ -1,9 +0,0 @@
-import pool from '../server/db.js';
-
-async function listPrograms() {
-  const progs = await pool.query('SELECT program_id, name FROM public.programs');
-  console.log('ALL PROGRAMS:', progs.rows);
-  process.exit(0);
-}
-
-listPrograms();
